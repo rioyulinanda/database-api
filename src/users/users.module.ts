cc9e37905422a64d7bc047370652a92e6f7267c8
Rio Y Kurniawan <rioyulinanda@gmail.com>
2023-05-09 20:00:06 +0800

    updating file

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,11 +3,10 @@ import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
 import { UsersRepository } from './users.repository';
 import { PrismaService } from '../prisma.service';
-import { ShoplistModule } from '../shoplist.module';
 
 @Module({
   controllers: [UsersController],
   providers: [UsersService, UsersRepository, PrismaService],
-  imports: [ShoplistModule],
+  imports: [],
 })
 export class UsersModule {}
